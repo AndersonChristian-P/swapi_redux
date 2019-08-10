63ab04927c394f34a404f7c7e50fa1c02cabab88
Christian Anderson <ca96187@gmail.com>
2019-08-09 19:08:34 -0600

    setup basic redux store and reducer

---
+++ src/redux/swapiReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  people: []
+}
+
+export default function swapiReducer(state = initialState, action) {
+  const { type, payload } = action
+  switch (type) {
+    default:
+      return state
+  }
+}
