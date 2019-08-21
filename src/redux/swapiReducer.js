149accbf79911de7505d32fc178e4fb6004e00c6
Christian Anderson <ca96187@gmail.com>
2019-08-21 14:06:50 -0600

    created constants file and put together the reducer

--- src/redux/swapiReducer.js
+++ src/redux/swapiReducer.js
@@ -2,9 +2,20 @@ const initialState = {
   people: []
 }
 
+const SET_CHARACTERS = "SET_CHARACTERS"
+
+export function getCharacters() {
+  return {
+    type: SET_CHARACTERS
+  }
+}
+
+
 export default function swapiReducer(state = initialState, action) {
   const { type, payload } = action
   switch (type) {
+    case SET_CHARACTERS:
+      return { ...state, payload }
     default:
       return state
   }
