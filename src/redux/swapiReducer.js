f1a40e0057018c80d40a78cb1690942f72bc61ca
Christian Anderson <ca96187@gmail.com>
2019-08-22 12:40:15 -0600

    coded out a basic reducer that just returns state

--- src/redux/swapiReducer.js
+++ src/redux/swapiReducer.js
@@ -1,23 +1,36 @@
 const initialState = {
-  people: []
+
 }
-
-const SET_CHARACTERS = "SET_CHARACTERS"
-
-export function getCharacters(people) {
-  return {
-    type: SET_CHARACTERS,
-    payload: people
-  }
-}
-
 
 export default function swapiReducer(state = initialState, action) {
   const { type, payload } = action
   switch (type) {
-    case SET_CHARACTERS:
-      return { ...state, people: payload }
     default:
       return state
   }
 }
+
+
+// const initialState = {
+//   people: []
+// }
+
+// const SET_CHARACTERS = "SET_CHARACTERS"
+
+// export function getCharacters(people) {
+//   return {
+//     type: SET_CHARACTERS,
+//     payload: people
+//   }
+// }
+
+
+// export default function swapiReducer(state = initialState, action) {
+//   const { type, payload } = action
+//   switch (type) {
+//     case SET_CHARACTERS:
+//       return { ...state, people: payload }
+//     default:
+//       return state
+//   }
+// }
