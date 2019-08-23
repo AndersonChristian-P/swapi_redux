d4c4700cc1afc398e49b65ef1bf7af1ab046f0ae
Christian Anderson <ca96187@gmail.com>
2019-08-23 17:04:02 -0600

    reset swapiReducer to base code and setup store to take in multiple reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,17 @@
-import { createStore } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
+import promiseMiddleware from "redux-promise-middleware"
 
 import swapiReducer from "./swapiReducer"
 
-const store = createStore(swapiReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// provide ability to use redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const enhancer = composeEnhancers(applyMiddleware(promiseMiddleware))
+
+const store = createStore(swapiReducer, enhancer)
+
 
 export default store
+
+
+// const store = createStore(swapiReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
--- src/redux/swapiReducer.js
+++ src/redux/swapiReducer.js
@@ -11,26 +11,26 @@ export default function swapiReducer(state = initialState, action) {
 }
 
 
-// const initialState = {
-//   people: []
-// }
+/* const initialState = {
+  people: []
+}
 
-// const SET_CHARACTERS = "SET_CHARACTERS"
+const SET_CHARACTERS = "SET_CHARACTERS"
 
-// export function getCharacters(people) {
-//   return {
-//     type: SET_CHARACTERS,
-//     payload: people
-//   }
-// }
+export function getCharacters(people) {
+  return {
+    type: SET_CHARACTERS,
+    payload: people
+  }
+}
 
 
-// export default function swapiReducer(state = initialState, action) {
-//   const { type, payload } = action
-//   switch (type) {
-//     case SET_CHARACTERS:
-//       return { ...state, people: payload }
-//     default:
-//       return state
-//   }
-// }
+export default function swapiReducer(state = initialState, action) {
+  const { type, payload } = action
+  switch (type) {
+    case SET_CHARACTERS:
+      return { ...state, people: payload }
+    default:
+      return state
+  }
+} */
